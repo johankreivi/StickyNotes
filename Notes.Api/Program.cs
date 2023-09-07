af5de3617d826a9791c14ed22f29b182ec18894a
0h4y <johan.kreivi@gmail.com>
2023-09-07 12:57:32 +0200

    TypeNameHandling changed to: None to prevent Insecure Deserialization whit newtonsoft

--- Notes.Api/Program.cs
+++ Notes.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Services
     .AddControllers()
     .AddNewtonsoftJson(options =>
     {
-        options.SerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+        options.SerializerSettings.TypeNameHandling = TypeNameHandling.None;
         options.SerializerSettings.Converters.Add(new StringEnumConverter());
     });
 
